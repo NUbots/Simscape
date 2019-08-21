138d3d95be78f1c381256b699c1b8080624927cd
MattAmos <matthewjamesamos@gmail.com>
2019-08-22 00:04:28 +1000

    Adds script files for static simulation

---
+++ src/kick.m
@@ -0,0 +1,23 @@
+zombie;
+
+% Mid kick
+l_hip_yaw = 0.02923973;
+r_hip_yaw = -0.02923973;
+r_shoulder_pitch = 1.963495;
+r_shoulder_roll = -0.1239184;
+r_elbow_pitch = -2.443461;
+l_shoulder_pitch = 1.963495;
+l_shoulder_roll = 0.1239184;
+l_elbow_pitch = -2.443461;
+r_ankle_pitch = -0.2393594;
+r_knee_pitch = 0.2561415;
+r_hip_pitch = -0.2040693;
+l_ankle_pitch = -0.5922612;
+l_knee_pitch = 0.897502;
+l_hip_pitch = -0.4741158;
+l_hip_roll = 0.4925281;
+l_ankle_roll = -0.3789858;
+r_hip_roll = 0.1703134;
+r_ankle_roll = -0.2040693;
+
+sim("NUgus_static", 0);
---
+++ src/squat.m
@@ -0,0 +1,25 @@
+zombie;
+
+% Goalie squat
+r_hip_pitch = -1.825883;
+l_hip_pitch = -1.953234;
+l_hip_yaw = 1.48679;
+r_hip_yaw = -1.522081;
+r_knee_pitch = 2.367414;
+l_knee_pitch = 2.373552;
+r_ankle_pitch = -0.5907269;
+l_ankle_pitch = -0.5400931;
+l_ankle_roll = 0.01227484;
+r_ankle_roll = -0.07211471;
+l_hip_roll = 0.02608404;
+r_hip_roll = 0.04296195;
+r_elbow_pitch = -0.5063373;
+l_shoulder_roll = 1.104544;
+r_shoulder_roll = -0.9940705;
+l_shoulder_pitch = 1.99447;
+r_shoulder_pitch = 2.034364;
+l_elbow_pitch = -0.5937955;
+head_pitch = 0.5907269;
+head_yaw = 0.02608404;
+
+sim("NUgus_static", 0);
