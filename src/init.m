9bd1041c687bbccd9194cf9d36d2d89a724688cd
MattAmos <matthewjamesamos@gmail.com>
2019-08-22 09:32:31 +1000

    Updates floor size to be configurable

--- src/init.m
+++ src/init.m
@@ -1,7 +1,10 @@
 % Define floor contact parameters
-floor.pos_gain = -1e5; % N/m 
-floor.vel_gain = -1e3; % Ns/m
-floor.friction = 0.6;  % Friction coeff.
+floor.length = 2;       % m (x)
+floor.width = 2;        % m (y)
+floor.depth = 0.01;     % m (z)
+floor.pos_gain = -1e5;  % N/m 
+floor.vel_gain = -1e3;  % Ns/m
+floor.friction = 0.6;   % Friction coeff.
 
 % Initialise servos
 l_shoulder_pitch = 0;
