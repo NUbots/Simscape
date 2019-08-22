767715aafac7344f94a822d754a4b64a6e491297
Matthew Amos <matthewjamesamos@gmail.com>
2019-08-22 22:58:04 +1000

    Remove per-servo initial positions from the initialisation file

--- src/init.m
+++ src/init.m
@@ -7,23 +7,5 @@ floor.vel_gain = -1e3;  % Ns/m
 floor.friction = 0.6;   % Friction coeff.
 
 % Initialise servos
-l_shoulder_pitch = 0;
-r_shoulder_pitch = 0;
-l_shoulder_roll = 0;
-r_shoulder_roll = 0;
-l_elbow_pitch = 0;
-r_elbow_pitch = 0;
-l_hip_yaw = 0;
-r_hip_yaw = 0;
-l_hip_roll = 0;
-r_hip_roll = 0;
-l_hip_pitch = 0;
-r_hip_pitch = 0;
-l_knee_pitch = 0;
-r_knee_pitch = 0;
-l_ankle_pitch = 0;
-r_ankle_pitch = 0;
-l_ankle_roll = 0;
-r_ankle_roll = 0;
-h_pitch = 0;
-h_yaw = 0;
+MX64_init;
+MX106_init;
